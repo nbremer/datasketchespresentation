0aebebd8eb58fc6dac7acdd591e065955713b90a
Nadieh Bremer <Nadieh@MacBook-Pro-van-Nadieh.local>
2017-07-12 09:36:40 +0200

    Updating to start on 40 minute version

--- js/reveal-to-d3-config.js
+++ js/reveal-to-d3-config.js
@@ -81,34 +81,34 @@ pt.slideIdToFunctions = {
       pt.chordToLoom2.moveApart(lotrWords);
     },
   },
-  // 'dbz-path-simple': {
-  //   'init': function() {
-  //     pt.dbzPathSimple.init(dbzFights);
-  //   },
-  //   '-1': function() {
-  //     pt.dbzPathSimple.showCirclesOnly();
-  //   },
-  //   0: function() {
-  //     pt.dbzPathSimple.showSimplePaths();
-  //   },
-  //   1: function() {
-  //     pt.dbzPathSimple.showAnchorPoints();
-  //   },
-  //   2: function() {
-  //     pt.dbzPathSimple.addSwoop();
-  //   },
-  //   3: function() {
-  //     pt.dbzPathSimple.addChangeSide();
-  //   },
-  //   4: function() {
-  //     pt.dbzPathSimple.hideAnchors();
-  //   },
-  // },
-  // 'dbz-path-final': {
-  //   'init': function() {
-  //     pt.dbzPathFinal.init(dbzFights);
-  //   }
-  // },
+  'dbz-path-simple': {
+    'init': function() {
+      pt.dbzPathSimple.init(dbzFights);
+    },
+    '-1': function() {
+      pt.dbzPathSimple.showCirclesOnly();
+    },
+    0: function() {
+      pt.dbzPathSimple.showSimplePaths();
+    },
+    1: function() {
+      pt.dbzPathSimple.showAnchorPoints();
+    },
+    2: function() {
+      pt.dbzPathSimple.addSwoop();
+    },
+    3: function() {
+      pt.dbzPathSimple.addChangeSide();
+    },
+    4: function() {
+      pt.dbzPathSimple.hideAnchors();
+    },
+  },
+  'dbz-path-final': {
+    'init': function() {
+      pt.dbzPathFinal.init(dbzFights);
+    }
+  },
   'in-english-network-bad': {
     'init': function() {
       pt.inEnglishNetwork.init(networkLinks, false, "in-english-network-bad", "inEnglishNetworkBad");
@@ -147,125 +147,6 @@ pt.slideIdToFunctions = {
   'end-slide': {
     'init': function() {
       pt.endSlide.init();
-    }
-  },
-  // Shirley slides
-  // 'sketch-lines': {
-  //   init: () => {
-  //     pt.sketchLines.init();
-  //   },
-  //   '-1': () => {
-  //     // all the lines positioned by character, and also their names
-  //     var animateBefore = true;
-  //     var triggerForce = true;
-  //     pt.sketchLines.drawLines(hamiltonAllLines, animateBefore, triggerForce);
-  //     pt.sketchLines.drawSongs(hamiltonCharacters, 'middle');
-  //     pt.sketchLines.drawStaffs([]);
-  //     pt.sketchLines.drawCurves([]);
-  //     pt.sketchLines.drawThemes([]);
-  //   },
-  //   0: () => {
-  //     // lines grouped consecutively
-  //     // positioned by character, and also their names
-  //     var animateBefore = true;
-  //     var triggerForce = true;
-  //     pt.sketchLines.drawLines(hamiltonGroupedLines, animateBefore, triggerForce);
-  //     pt.sketchLines.drawSongs(hamiltonCharacters, 'middle');
-  //     pt.sketchLines.drawStaffs([]);
-  //     pt.sketchLines.drawCurves([]);
-  //     pt.sketchLines.drawThemes([]);
-  //   },
-  //   1: () => {
-  //     // fade in sketch notes, do no animating
-  //     var animateBefore = false;
-  //     var triggerForce = true;
-  //     pt.sketchLines.drawLines(hamiltonGroupedLines, animateBefore, triggerForce);
-  //     pt.sketchLines.drawSongs(hamiltonCharacters, 'middle');
-  //     pt.sketchLines.drawStaffs([]);
-  //     pt.sketchLines.drawCurves([]);
-  //     pt.sketchLines.drawThemes([]);
-  //     // so lower opacity of everything
-  //     pt.sketchLines.lowerOpacity();
-  //   },
-  //   2: () => {
-  //     pt.sketchLines.drawSongs([], 'start');
-  //     pt.sketchLines.drawStaffs([]);
-  //     pt.sketchLines.drawCurves([]);
-  //     pt.sketchLines.drawThemes([]);
-  //
-  //     var animateBefore = false;
-  //     var triggerForce = true;
-  //     // fade out sketch notes and first try at rectangular lines
-  //     // don't animate path before re-positioning
-  //     pt.sketchLines.drawLines(hamiltonLines, animateBefore, triggerForce, () => {
-  //       pt.sketchLines.drawSongs(hamiltonSongs, 'start');
-  //     });
-  //   },
-  //   3: () => {
-  //     // add in diamond theme, but nothing else should animate
-  //     var animateBefore = false;
-  //     var triggerForce = false;
-  //     pt.sketchLines.drawLines(hamiltonLines, animateBefore, triggerForce)
-  //     pt.sketchLines.drawSongs(hamiltonSongs, 'start');
-  //     pt.sketchLines.drawStaffs([]);
-  //     pt.sketchLines.drawCurves([]);
-  //     pt.sketchLines.drawThemes(hamiltonThemes);
-  //   },
-  //   4: () => {
-  //     // fade in second sketch note, do no animating
-  //     var animateBefore = false;
-  //     var triggerForce = true;
-  //     pt.sketchLines.drawLines(hamiltonLines, animateBefore, triggerForce)
-  //     pt.sketchLines.drawSongs(hamiltonSongs, 'start');
-  //     pt.sketchLines.drawStaffs([]);
-  //     pt.sketchLines.drawCurves([]);
-  //     pt.sketchLines.drawThemes(hamiltonThemes);
-  //     pt.sketchLines.lowerOpacity();
-  //   },
-  //   5: () => {
-  //     pt.sketchLines.drawSongs([], 'start');
-  //     pt.sketchLines.drawThemes([]);
-  //
-  //     // fade out sketch notes, animate in final lines and background staff and themes
-  //     var animateBefore = false;
-  //     var triggerForce = true;
-  //     pt.sketchLines.drawLines(hamiltonFinalLines, animateBefore, triggerForce, () => {
-  //       pt.sketchLines.drawSongs(hamiltonFinalSongs, 'start');
-  //       pt.sketchLines.drawStaffs(hamiltonFinalSongs);
-  //       pt.sketchLines.drawCurves(hamiltonFinalThemes);
-  //     });
-  //   },
-  //   6: () => {
-  //     // take away staff finally
-  //     var animateBefore = false;
-  //     var triggerForce = false;
-  //     pt.sketchLines.drawLines(hamiltonFinalLines, animateBefore, triggerForce);
-  //     pt.sketchLines.drawSongs(hamiltonFinalSongs, 'start');
-  //     pt.sketchLines.drawCurves(hamiltonFinalThemes);
-  //     pt.sketchLines.drawStaffs([]);
-  //   },
-  // },
-  'code-flowers-svg': {
-    init: () => {
-      pt.codeFlowers.init();
-    },
-    '-1': () => {
-      pt.codeFlowers.timeline.tweenTo('one');
-    },
-    0: () => {
-      pt.codeFlowers.timeline.tweenTo('one+=' + pt.codeFlowers.duration);
-    },
-    1: () => {
-      pt.codeFlowers.timeline.tweenTo('two+=' + pt.codeFlowers.duration);
-    },
-    2: () => {
-      pt.codeFlowers.timeline.tweenTo('three+=' + pt.codeFlowers.duration);
-    },
-    3: () => {
-      pt.codeFlowers.timeline.tweenTo('five');
-    },
-    4: () => {
-      pt.codeFlowers.timeline.tweenTo('five+=' + (pt.codeFlowers.duration / 2));
     }
   },
 };
