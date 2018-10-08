c56c6e0c9f27a0af4aeaf018842ff2279ba1e8c5
nbremer <nadiehbremer@gmail.com>
2018-10-08 08:49:56 +0200

    Updated with my final month

--- slides/slide_N_48/datasketchesTitle.js
+++ slides/slide_N_48/datasketchesTitle.js
@@ -2,7 +2,7 @@ pt.datasketchesTitle = pt.datasketchesTitle || {};
 
 pt.datasketchesTitle.init = function() {
 
-	var ids = ["july","august","september","october","november","december","january","february","march","april","june"];
+	var ids = ["july","august","september","october","november","december","january","february","march","april","may","june"];
 	var counter = 0;
 	var dur = 1000;
 
